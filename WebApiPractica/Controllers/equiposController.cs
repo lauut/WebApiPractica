82fab27b92a371eec10980cf013b657c899931f6
MINEDUCYT <MINEDUCYT@DESKTOP-DHSOO9J>
2024-02-22 17:42:53 -0600

    Agregar archivos de proyecto.

---
+++ WebApiPractica/Controllers/equiposController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiPractica.Models;
+
+namespace WebApiPractica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+        public equiposController(equiposContext equiposContext)
+        {
+            _equiposContext = equiposContext;
+        }
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            try
+            {
+
+            List<equipos> listadoEquipos = (from e in _equiposContext.equipos
+                                            select e).ToList();
+            if(listadoEquipos.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoEquipos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ equiposContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFramework.SqlServer;
+namespace WebApiPractica.Models
+
+public class equiposContext : DbContext
+{
+	public equiposContext(DbContextequipos<equiposContext> options) : base(options) 
+	{ 
+
+	}
+}
